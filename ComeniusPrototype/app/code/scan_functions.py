1e8ce831f3da40fe59ec07eb0e6336fdd390cc08
Jelmer-ja <jelmer-ja@hotmail.com>
2023-01-07 19:13:02 +0100

    Ancova fix v2

--- ComeniusPrototype/app/code/scan_functions.py
+++ ComeniusPrototype/app/code/scan_functions.py
@@ -1003,7 +1003,7 @@ class ScanFunctions:
         output += '<br>'+'<br>'.join(self.detect_report_stat(doc, 'p', solution['p'][3]))
         output += '<br>'+'<br>'.join(self.detect_report_stat(doc, 'eta<sup>2</sup>', solution['eta'][3], aliases=['eta2','eta']))
         
-        between_sent = [x for x in doc.sents if (lef(solution['independent'].get_all_syns(),[y.text for y in x]) or 'between-subject' in x.text) and ('significant' in x.text or 'effect' in x.text) and not any([x in text for x in markers[1]])]
+        between_sent = [x for x in doc.sents if (lef(solution['independent'].get_all_syns(),[y.text for y in x]) or 'between-subject' in x.text) and ('significant' in x.text or 'effect' in x.text) and not any([y.text in markers[1] for y in x])]
         if between_sent != []:
             output += '<br>'+'<br>'.join(self.detect_decision_multirm(between_sent[0], solution, solution['independent'].name, ['between-subject'], solution['p'][2],solution['eta'][2]))
             if(solution['p'][2] < 0.05):
