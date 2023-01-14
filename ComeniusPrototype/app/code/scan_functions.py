96df1e765ae473eef2dfe42f7490a5d7ee7cc269
Jelmer-ja <jelmer-ja@hotmail.com>
2023-01-14 21:30:03 +0100

    design attempted fix v3

--- ComeniusPrototype/app/code/scan_functions.py
+++ ComeniusPrototype/app/code/scan_functions.py
@@ -744,19 +744,24 @@ class ScanFunctions:
             scorepoints['indcorrect'] = factor_roles[0] in indep_span or 'factor' in indep_span
             if solution['assignment_type'] == 5 or solution['assignment_type'] == 13:
                 scorepoints['factor1'] = 'within' in indep_span
-        if solution['assignment_type'] == 13 or solution['assignment_type'] == 4:   
+        if solution['assignment_type'] == 13:   
             ind2token = [x for x in tokens if simple_lef(solution['independent2'].get_all_syns(),x)]
             if ind2token != []:
                 scorepoints['ind2'] = True
                 token2_location = tokens.index(ind2token[0])
-                token2_sent = [x for x in doc.text.split('. ') if simple_lef(solution['independent2'].get_all_syns(),x)]
                 indep2_span = ' '.join(tokens[token2_location - 2:token2_location + 4])
-                scorepoints['ind2correct'] = (factor_roles[0] in token2_sent[0] or 'factor' in token2_sent[0]) and not (factor_roles[1] in token2_sent[0])
+                scorepoints['ind2correct'] = (factor_roles[0] in doc.text or 'factor' in doc.text) and not (factor_roles[1] in doc.text)
                 if solution['assignment_type'] == 13:
                     scorepoints['factor2'] = 'between' in indep2_span
+        elif solution['assignment_type'] == 4:
+            ind2token = [x for x in tokens if simple_lef(solution['independent2'].get_all_syns(),x)]
+            if ind2token != []:
+                token2_sent = [x for x in doc.text.split('. ') if simple_lef(solution['independent2'].get_all_syns(),x)]
+                scorepoints['ind2correct'] = (factor_roles[0] in token2_sent or 'factor' in token2_sent) and not (factor_roles[1] in token2_sent)
+                if solution['assignment_type'] == 13:
+                    scorepoints['factor2'] = 'between' in token2_sent
         else:
-            scorepoints['ind2'] = True
-            scorepoints['ind2correct'] = True
+            scorepoints['ind2'] = True;scorepoints['ind2correct'] = True
         deps = [x for x in doc.sents if lef(solution['dependent'].get_all_syns(),[y.text for y in x])]
         if deps != []:
             scorepoints['dep'] = True
