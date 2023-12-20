1bd5bd6b81ed4b03656ea95dff0a422dba71a08e
JulesEllis <40783454+JulesEllis@users.noreply.github.com>
2023-12-20 23:41:54 +0100

    Copyright
    Copyright added

--- ComeniusPrototype/app/forms.py
+++ ComeniusPrototype/app/forms.py
@@ -1,3 +1,12 @@
+#    This project contains CASWIC: Coaching App for Statistical Writing in Introductory Course.
+#    Copyright (C) 2023 Jules Ellis and Jelmer Jansen
+#
+#    This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+#
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms import TextAreaField, SelectField
